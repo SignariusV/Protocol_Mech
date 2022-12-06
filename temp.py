871890ce30dbf98e4db08c990c0b976e968442be
Vertaev <vertaiev@gmail.com>
2022-12-06 14:06:38 +0300

    добил проверку СИ

--- temp.py
+++ temp.py
@@ -1,13 +1,44 @@
-from docxtpl import DocxTemplate
+import os
+import re
+from os import listdir
+from win32com import client as wc
 from docx import Document
-file='Doc1.docx'
-
-doc = Document(file)
-doc.add_paragraph('')
-doc.save(file)
 
 
-tpl = DocxTemplate(file)
-context = {}
-tpl.render(context)
-tpl.save(file)
+
+def convert_docx(path_in, path_out):
+    w = wc.Dispatch('Word.Application')
+    doc = w.Documents.Open(path_in)
+    doc.SaveAs(path_out + '.docx', 16)
+    doc.Close()
+    w.Quit()
+
+
+def check_metr(name, protocol_path):
+    path='X:\ИЦ Омега\Метрология\Формы 1,2,3,4,6_Паспорт ИЦ Омега'
+    if os.path.isdir(path):
+        for file in listdir(path):
+            if file.endswith('doc') and not file.startswith('~') and name in file:
+                try:
+                    convert_docx( f'{path}\{file}', name)
+                except:
+                    print('Не удалось найти нужные файлы на сервере')
+                break
+    if name=='СИ':
+        table=3
+    elif name=='ИО':
+        table=4
+    protocol = Document(protocol_path)
+    metr_form = Document(f"C:\\Users\\Vertaev\\Documents\\{name}.docx")
+    for row in protocol.tables[table].rows[1:]:
+        for form_row in metr_form.tables[1].rows:
+            if row.cells[0].text in form_row.cells[2].text and row.cells[2].text in form_row.cells[4].text:
+                row.cells[4].text = re.findall(r"\d{2}\.\d{2}\.\d{4}", form_row.cells[7].text)[-1]
+                print(row.cells[4].text)
+                row.cells[4].paragraphs[0].style = 'Табл. по центру обычный'
+                break
+
+
+name = 'СИ'
+protocol_path='Doc.docx'
+check_metr(name, protocol_path)
